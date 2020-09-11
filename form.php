31f2b2a0b08b61ab268d2867387df9172f2341a5
Md.Rabbi Hossain <63368034+mdrabbihossain@users.noreply.github.com>
2020-09-11 12:38:11 +0600

    Add files via upload

---
+++ form.php
@@ -0,0 +1,135 @@
+<?php 
+	$error="";
+	if (!empty($_POST)){
+		if (empty($_POST['Name'])){
+			$error=$error."Name field is required!<br>";
+		}
+		if (empty($_POST['mail'])){
+			$error=$error."E-mail field is required!<br>";
+		}
+		if (empty($_POST['phone'])){
+			$error=$error."Phone field is required!<br>";
+		}
+		if (empty($_POST['pass1'])){
+			$error=$error."Password field is required!<br>";
+		}
+		elseif (strlen($_POST['pass1'])<5){
+			# code...
+			$error=$error."Password should be at least 5 charcters long! <br>";
+		}
+		elseif ($_POST['pass1']!=$_POST['pass2']){
+			# code...
+			$error=$error."Confirm password did not match! <br>";
+		}
+		// if (empty($_POST['gender'])){
+		// 	$error=$error."Gender field is required! <br>";
+		// }
+		if (empty($error)){
+			echo "Successful";
+			$_POST=array();
+		}
+	}
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>project2</title>
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<!-- fontAwesome -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="form.css">
+	<style type="text/css" media="screen">
+		td span{
+			color: red;
+		}
+	</style>
+</head>
+<body>
+	<form action="" method="POST" accept-charset="utf-8">
+		<div class="" id="mainForm">
+			<div class="" id="error">
+				<?=$error ?>
+			</div>
+		<table>
+				<tr>
+					<td>Name <span>*</span></td>
+					<td><input type="text" name="Name"></td>
+				</tr>
+				<tr>
+					<td>E-mail <span>*</span></td>
+					<td><input type="mail" name="mail"></td>
+				</tr>
+				<tr>
+					<td>Phone Number <span>*</span></td>
+					<td><input type="number" name="phone"></td>
+				</tr>
+				<tr>
+					<td>Website </td>
+					<td><input type="text" name="Website"></td>
+				</tr>
+				<tr>
+					<td>Date of birth<span>*</span></td>
+					<td><input type="date" name="dob"></td>
+				</tr>
+				<tr>
+					<td>Password <span>*</span></td>
+					<td><input type="password" name="pass1"></td>
+				</tr>
+				<tr>
+					<td>Confirm Password <span>*</span></td>
+					<td><input type="password" name="pass2"></td>
+				</tr>
+				<tr>
+					<td>Address <span>*</span></td>
+					<td> <textarea name="address"></textarea></td>
+				</tr>
+				<tr>
+					<td>Division</td>
+					<td>
+						<select name="division">
+							<option>-Select Division-</option>
+							<option>Dhaka</option>
+							<option>Rajshahic</option>
+							<option>Khulna</option>
+							<option>Barishal</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>Gender</td>
+					<td>
+						<label><input type="radio" name="gender" value="">Male</label>
+						<label><input type="radio" name="gender" value="">Female</label>
+					</td>
+				</tr>
+				<tr>
+					<td>Interest</td>
+					<td>
+						<label><input type="checkbox" name="interest[]" value="gane">Playing Game</label>
+						<label><input type="checkbox" name="interest[]" value="singing">Singing</label>
+						<label><input type="checkbox" name="interest[]" value="travelling">Travelling</label>
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><button type="submit">Submit</button></td>
+				</tr>
+			</tbody>
+		</table>
+		</div>
+	</form>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
